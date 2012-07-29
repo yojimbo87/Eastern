c676618d0e7701d3e8463b74e7dbf5495c7e4c9a
Tomas Bosak <bosak.tomas@gmail.com>
2012-07-29 12:24:18 +0200

    Make EasternClient static class.

--- Eastern/EasternClient.cs
+++ Eastern/EasternClient.cs
@@ -3,29 +3,16 @@ using Eastern.Protocol.Operations;
 
 namespace Eastern
 {
-    public class EasternClient
+    public static class EasternClient
     {
         internal static string DriverName { get { return "Eastern"; } }
         internal static string DriverVersion { get { return "0.0.1 pre-alpha"; } }
         internal static short ProtocolVersion { get { return 12; } }
         internal static string ClientID { get { return "null"; } }
 
-        private Worker WorkerConnection { get; set; }
-
-        public EasternClient()
+        static EasternClient()
         {
-            WorkerConnection = new Worker();
+            
         }
-
-        public EasternClient(string hostname, int port)
-        {
-            WorkerConnection = new Worker();
-            WorkerConnection.Initialize(hostname, port);
-        }
-
-        /*internal static object QueueOperation<T>(T operation)
-        {
-            return WorkerConnection.ExecuteOperation<T>(operation);
-        }*/
     }
 }
