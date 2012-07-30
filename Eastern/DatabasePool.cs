b803e8b7f7310a394d92e9bad7c7849fcae5b434
Tomas Bosak <bosak.tomas@gmail.com>
2012-07-30 21:37:29 +0200

    Initial implementation of simple connection pooling.

---
+++ Eastern/DatabasePool.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace Eastern
+{
+    internal class DatabasePool
+    {
+        private string Hostname { get; set; }
+        private int Port { get; set; }
+        private string DatabaseName { get; set; }
+        private ODatabaseType DatabaseType { get; set; }
+        private string UserName { get; set; }
+        private string UserPassword { get; set; }
+
+        internal string PoolHash
+        {
+            get
+            {
+                return Hostname + Port + DatabaseName + DatabaseType.ToString() + UserName;
+            }
+        }
+
+        internal Queue<ODatabase> Databases { get; set; }
+
+        internal DatabasePool(string hostname, int port, string databaseName, ODatabaseType databaseType, string userName, string userPassword)
+        {
+            Hostname = hostname;
+            Port = port;
+            DatabaseName = databaseName;
+            DatabaseType = databaseType;
+            UserName = userName;
+            UserPassword = userPassword;
+            Databases = new Queue<ODatabase>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                ODatabase database = new ODatabase(Hostname, Port, DatabaseName, DatabaseType, UserName, UserPassword);
+                database.ReturnToPool = true;
+
+                Databases.Enqueue(database);
+            }
+        }
+    }
+}
--- Eastern/EasternClient.cs
+++ Eastern/EasternClient.cs
@@ -1,10 +1,14 @@
 ﻿using Eastern.Connection;
 using Eastern.Protocol.Operations;
+using System.Collections.Generic;
 
 namespace Eastern
 {
     public static class EasternClient
     {
+        private static object SyncRoot { get; set; }
+        private static List<DatabasePool> DatabasePools { get; set; }
+
         internal static string DriverName { get { return "Eastern"; } }
         internal static string DriverVersion { get { return "0.0.1 pre-alpha"; } }
         internal static short ProtocolVersion { get { return 12; } }
@@ -12,7 +16,62 @@ namespace Eastern
 
         static EasternClient()
         {
-            
+            SyncRoot = new object();
+            DatabasePools = new List<DatabasePool>();
+        }
+
+        public static void CreateDatabasePool(string hostname, int port, string databaseName, ODatabaseType databaseType, string userName, string userPassword)
+        {
+            lock (SyncRoot)
+            {
+                DatabasePool pool = new DatabasePool(hostname, port, databaseName, databaseType, userName, userPassword);
+
+                DatabasePools.Add(pool);
+            }
+        }
+
+        public static ODatabase GetDatabase(string hostname, int port, string databaseName, ODatabaseType databaseType, string userName, string userPassword)
+        {
+            lock (SyncRoot)
+            {
+                ODatabase database;
+                string hash = hostname + port + databaseName + databaseType.ToString() + userName;
+
+                if (DatabasePools.Exists(q => q.PoolHash == hash))
+                {
+                    DatabasePool pool = DatabasePools.Find(q => q.PoolHash == hash);
+
+                    if (pool.Databases.Count > 0)
+                    {
+                        database = pool.Databases.Dequeue();
+                        database.Reload();
+
+                        return database;
+                    }
+                }
+
+                database = new ODatabase(hostname, port, databaseName, databaseType, userName, userPassword);
+                database.ReturnToPool = false;
+
+                return database;
+            }
+        }
+
+        internal static void ReturnDatabase(ODatabase database)
+        {
+            lock (SyncRoot)
+            {
+                if (DatabasePools.Exists(q => q.PoolHash == database.Hash) && database.ReturnToPool)
+                {
+                    DatabasePool pool = DatabasePools.Find(q => q.PoolHash == database.Hash);
+
+                    pool.Databases.Enqueue(database);
+                }
+                else
+                {
+                    database.Close();
+                }
+            }
         }
     }
 }
