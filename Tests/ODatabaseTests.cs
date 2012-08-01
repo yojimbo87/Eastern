021a4a2149ef9df72306afdc9c956fa5bb130c87
Tomas Bosak <bosak.tomas@gmail.com>
2012-08-01 23:05:11 +0200

    Prepare testing ground for ODatabase API.

---
+++ Tests/ODatabaseTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class ODatabaseTests
+    {
+        private const string _hostname = "127.0.0.1";
+        private const int _port = 2424;
+        private const string _databaseName = "test1";
+        private const string _username = "admin";
+        private const string _password = "admin";
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
