82655ab9a91ccc488ebee363d22a009b9dde1901
Tomas Bosak <bosak.tomas@gmail.com>
2012-08-01 23:04:52 +0200

    Get rid of unnecessary code.

--- Tests/OServerTests.cs
+++ Tests/OServerTests.cs
@@ -7,9 +7,6 @@ using Eastern;
 
 namespace Tests
 {
-    /// <summary>
-    /// Summary description for UnitTest1
-    /// </summary>
     [TestClass]
     public class OServerTests
     {
@@ -17,58 +14,9 @@ namespace Tests
         private const int _port = 2424;
         private const string _rootName = "root";
         private const string _rootPassword = "9F696830A58E8187F6CC36674C666AE73E202DE3B0216C5B8BF8403C276CEE52";
-        private const string _databaseName = "test1";
         private const string _username = "admin";
         private const string _password = "admin";
-
         private const string _newDatabaseName = "testTempNewDatabase001x";
-
-        public OServerTests()
-        {
-            //
-            // TODO: Add constructor logic here
-            //
-        }
-
-        private TestContext testContextInstance;
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-
-        #region Additional test attributes
-        //
-        // You can use the following additional attributes as you write your tests:
-        //
-        // Use ClassInitialize to run code before running the first test in the class
-        // [ClassInitialize()]
-        // public static void MyClassInitialize(TestContext testContext) { }
-        //
-        // Use ClassCleanup to run code after all tests in a class have run
-        // [ClassCleanup()]
-        // public static void MyClassCleanup() { }
-        //
-        // Use TestInitialize to run code before running each test 
-        // [TestInitialize()]
-        // public void MyTestInitialize() { }
-        //
-        // Use TestCleanup to run code after each test has run
-        // [TestCleanup()]
-        // public void MyTestCleanup() { }
-        //
-        #endregion
 
         [TestMethod]
         public void TestConnect()
@@ -99,7 +47,7 @@ namespace Tests
         {
             using (OServer connection = new OServer(_hostname, _port, _rootName, _rootPassword))
             {
-                Assert.IsTrue(connection.DatabaseExist("test1"));
+                Assert.IsTrue(connection.DatabaseExist("demo"));
             }
         }
 
@@ -108,7 +56,7 @@ namespace Tests
         {
             using (OServer connection = new OServer(_hostname, _port, _rootName, _rootPassword))
             {
-                Assert.IsFalse(connection.DatabaseExist("thisShoulNotExist001x"));
+                Assert.IsFalse(connection.DatabaseExist("whateverThisShouldNotExit"));
             }
         }
 
