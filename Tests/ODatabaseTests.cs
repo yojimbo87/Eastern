70778e84f80727428df2dac17ec127d66b50f3de
Tomas Bosak <bosak.tomas@gmail.com>
2012-08-02 09:17:26 +0200

    Create tests for the rest of existing public API.

--- Tests/ODatabaseTests.cs
+++ Tests/ODatabaseTests.cs
@@ -44,5 +44,96 @@ namespace Tests
                 Assert.IsTrue(database.SessionID == -1);
             }
         }
+
+        [TestMethod]
+        public void TestDatabaseReload()
+        {
+            using (ODatabase database = new ODatabase(_hostname, _port, _databaseName, ODatabaseType.Document, _username, _password))
+            {
+                Assert.IsTrue(database.ClustersCount > 0);
+
+                database.Reload();
+
+                Assert.IsTrue(database.ClustersCount > 0);
+            }
+        }
+
+        [TestMethod]
+        public void TestDatabaseSize()
+        {
+            using (ODatabase database = new ODatabase(_hostname, _port, _databaseName, ODatabaseType.Document, _username, _password))
+            {
+                Assert.IsTrue(database.Size > 0);
+            }
+        }
+
+        [TestMethod]
+        public void TestDatabaseRecordsCount()
+        {
+            using (ODatabase database = new ODatabase(_hostname, _port, _databaseName, ODatabaseType.Document, _username, _password))
+            {
+                Assert.IsTrue(database.RecordsCount > 0);
+            }
+        }
+
+        [TestMethod]
+        public void TestDatabaseClusterAddRemove()
+        {
+            using (ODatabase database = new ODatabase(_hostname, _port, _databaseName, ODatabaseType.Document, _username, _password))
+            {
+                OCluster newCluster = database.AddCluster(OClusterType.Physical, "tempClusterTest3");
+
+                Assert.IsTrue(newCluster.ID >= 0);
+                Assert.IsTrue(!string.IsNullOrEmpty(newCluster.Name));
+                Assert.IsTrue(newCluster.RecordsCount == 0);
+
+                database.Reload();
+
+                OCluster cluster = database.Clusters.Find(x => x.ID == newCluster.ID);
+
+                Assert.IsTrue(cluster.ID >= newCluster.ID);
+                Assert.IsTrue(cluster.Name.Equals(newCluster.Name, StringComparison.CurrentCultureIgnoreCase));
+                Assert.IsTrue(cluster.RecordsCount == newCluster.RecordsCount);
+
+                database.RemoveCluster(newCluster.ID);
+
+                OCluster cluster2 = database.Clusters.Find(x => x.ID == newCluster.ID);
+
+                Assert.IsNull(cluster2);
+
+                database.Reload();
+
+                OCluster cluster3 = database.Clusters.Find(x => x.ID == newCluster.ID);
+
+                Assert.IsNull(cluster3);
+            }
+        }
+
+        [TestMethod]
+        public void TestDatabaseClusterRecordsCount()
+        {
+            using (ODatabase database = new ODatabase(_hostname, _port, _databaseName, ODatabaseType.Document, _username, _password))
+            {
+                foreach (OCluster cluster in database.Clusters)
+                {
+                    Assert.IsTrue(cluster.RecordsCount >= 0);
+                }
+            }
+        }
+
+        [TestMethod]
+        public void TestDatabaseClusterDataRange()
+        {
+            using (ODatabase database = new ODatabase(_hostname, _port, _databaseName, ODatabaseType.Document, _username, _password))
+            {
+                foreach (OCluster cluster in database.Clusters)
+                {
+                    long[] range = cluster.DataRange;
+
+                    Assert.IsTrue(range[0] >= -1);
+                    Assert.IsTrue(range[1] >= -1);
+                }
+            }
+        }
     }
 }
