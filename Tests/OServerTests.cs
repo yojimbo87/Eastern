db093b40bbf2256157013678ce540ba0d191aa6a
Tomas Bosak <bosak.tomas@gmail.com>
2012-08-21 13:50:34 +0200

    Fix typo.

--- Tests/OServerTests.cs
+++ Tests/OServerTests.cs
@@ -13,7 +13,8 @@ namespace Tests
         private const string _hostname = "127.0.0.1";
         private const int _port = 2424;
         private const string _rootName = "root";
-        private const string _rootPassword = "9F696830A58E8187F6CC36674C666AE73E202DE3B0216C5B8BF8403C276CEE52";
+        //private const string _rootPassword = "9F696830A58E8187F6CC36674C666AE73E202DE3B0216C5B8BF8403C276CEE52";
+        private const string _rootPassword = "root";
         private const string _username = "admin";
         private const string _password = "admin";
         private const string _newDatabaseName = "tempEasternUniqueTestDatabase0001x";
@@ -56,7 +57,7 @@ namespace Tests
         {
             using (OServer connection = new OServer(_hostname, _port, _rootName, _rootPassword))
             {
-                Assert.IsFalse(connection.DatabaseExist("whateverThisShouldNotExit001x"));
+                Assert.IsFalse(connection.DatabaseExist("whateverThisShouldNotExist001x"));
             }
         }
 
