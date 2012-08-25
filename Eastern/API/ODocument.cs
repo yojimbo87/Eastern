21b38aa6ac1c87c7b036d0d6af0a974f4c387ef1
Tomas Bosak <bosak.tomas@gmail.com>
2012-08-25 12:03:29 +0200

    Create ODocument and RecordParser classes.

---
+++ Eastern/API/ODocument.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Eastern
+{
+    public class ODocument
+    {
+        public string Class { get; set; }
+        public int Version { get; set; }
+        public Dictionary<string, object> Fields { get; set; }
+
+        public ODocument()
+        {
+            Fields = new Dictionary<string, object>();
+        }
+    }
+}
---
+++ Eastern/Protocol/RecordParser.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Eastern.Protocol
+{
+    internal static class RecordParser
+    {
+        internal static ODocument ToDocument(int version, string rawDocument)
+        {
+            ODocument document = new ODocument();
+            document.Version = version;
+
+            int atIndex = rawDocument.IndexOf('@');
+            int colonIndex = rawDocument.IndexOf(':');
+
+            if ((atIndex != -1) && (atIndex < colonIndex))
+            {
+                document.Class = rawDocument.Substring(0, atIndex);
+            }
+
+            return document;
+        }
+    }
+}
