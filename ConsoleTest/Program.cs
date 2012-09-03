f2985c0f820827f2743298928322390a610fab41
Tomas Bosak <bosak.tomas@gmail.com>
2012-09-03 21:51:55 +0200

    Experiment with new approach to document parsing.

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -20,12 +20,8 @@ namespace ConsoleTest
 
         static void Main(string[] args)
         {
-            string raw = "Profile@nick:\"ThePr,whoa:esident\",follows:[\"me\",\"you\"],followers:[#10:5,#10:6],name:\"Barack\",surname:\"Obama\",location:#3:2,invitedBy:,salary_cloned:,salary:120.3f";
+            string raw = "Profile@nick:\"ThePr,whoa:esident\",follows:(moe:#1:1,joe:#1:3),followers:(moe:\"whoa:,\"),name:\"Barack\",surname:\"Obama\",location:#3:2,invitedBy:,salary_cloned:";
             ORecord record = new ORecord(ORecordType.Document, 0, UTF8Encoding.UTF8.GetBytes(raw));
-            PrintDocument(raw, record.ToDocument());
-
-            raw = "nick:[(nick1:\"xxx\"),(nick2:\"yyy\")],joe:[(joe_1_1:\"xxx\",joe_1_2:\"yyy\")],moe:[(moe_1_1:#3:23,moe_1_2:\",whoa:#1:3,\",moe_1_3:#3:43,moe_1_4:[#124:34433],moe_1_5:[#124:344,#344:23])]";
-            record = new ORecord(ORecordType.Document, 0, UTF8Encoding.UTF8.GetBytes(raw));
             PrintDocument(raw, record.ToDocument());
 
             raw = "nick:";
@@ -33,6 +29,10 @@ namespace ConsoleTest
             PrintDocument(raw, record.ToDocument());
 
             raw = "nick:,joe:";
+            record = new ORecord(ORecordType.Document, 0, UTF8Encoding.UTF8.GetBytes(raw));
+            PrintDocument(raw, record.ToDocument());
+
+            /*raw = "nick:[(nick1:\"xxx\"),(nick2:\"yyy\")],joe:[(joe_1_1:\"xxx\",joe_1_2:\"yyy\")],moe:[(moe_1_1:#3:23,moe_1_2:\",whoa:#1:3,\",moe_1_3:#3:43,moe_1_4:[#124:34433],moe_1_5:[#124:344,#344:23])]";
             record = new ORecord(ORecordType.Document, 0, UTF8Encoding.UTF8.GetBytes(raw));
             PrintDocument(raw, record.ToDocument());
 
@@ -44,6 +44,10 @@ namespace ConsoleTest
             record = new ORecord(ORecordType.Document, 0, UTF8Encoding.UTF8.GetBytes(raw));
             PrintDocument(raw, record.ToDocument());
 
+            raw = "kyle:[(kyle1:13b),(kyle2:45b),(kyle3:244f)],joe:[(joe1_1:[1,2,4],joe1_2:12b),(joe2_1:3443.334,joe2_2:[\"asd\",\"fds\"])],hoe:(goe:(shmoe:4))";
+            record = new ORecord(ORecordType.Document, 0, UTF8Encoding.UTF8.GetBytes(raw));
+            PrintDocument(raw, record.ToDocument());*/
+
             Console.ReadLine();
         }
 
@@ -52,6 +56,7 @@ namespace ConsoleTest
             Console.WriteLine("Raw string: {0}", raw);
             Console.WriteLine("---------------------------------------------");
             Console.WriteLine("Version: {0}, Class name: {1}", document.Version, document.Class);
+            Console.WriteLine("---------------------------------------------");
 
             foreach (KeyValuePair<string, object> kv in document.Fields)
             {
@@ -75,6 +80,17 @@ namespace ConsoleTest
                     }
 
                     Console.WriteLine();
+                }
+                else if (kv.Value.GetType() == typeof(Dictionary<string, object>))
+                {
+                    Console.WriteLine("- {0} (ED): ", kv.Key);
+
+                    foreach (KeyValuePair<string, object> kve in (Dictionary<string, object>)kv.Value)
+                    //for (int i = 0; i < ((Dictionary<string, object>)kv.Value).Count; i++)
+                    //foreach (string value in (List<String>)kv.Value)
+                    {
+                        Console.WriteLine("  - {0}: {1}", kve.Key, kve.Value);
+                    }
                 }
                 else
                 {
--- Eastern/API/ORecord.cs
+++ Eastern/API/ORecord.cs
@@ -1,9 +1,13 @@
-﻿using Eastern.Protocol;
+﻿using System.Collections.Generic;
+using Eastern.Protocol;
 
 namespace Eastern
 {
     public class ORecord
     {
+        private ODocument Document { get; set; }
+        private string RawDocument { get; set; }
+
         public ORecordType Type { get; set; }
         public int Version { get; set; }
         public byte[] Content { get; set; }
@@ -24,17 +28,149 @@ namespace Eastern
 
         public ODocument ToDocument()
         {
-            if (Type == ORecordType.Document)
-            {
-                ODocument document = RecordParser.ToDocument(BinaryParser.ToString(Content));
-                document.Version = Version;
-
-                return document;
-            }
-            else
+            if (Type != ORecordType.Document)
             {
                 return null;
             }
+
+            Document = new ODocument();
+            Document.Version = Version;
+            RawDocument = BinaryParser.ToString(Content);
+
+            int atIndex = RawDocument.IndexOf('@');
+            int colonIndex = RawDocument.IndexOf(':');
+            int index = 0;
+
+            // parse class name
+            if ((atIndex != -1) && (atIndex < colonIndex))
+            {
+                Document.Class = RawDocument.Substring(0, atIndex);
+                index = atIndex + 1;
+            }
+            else
+            {
+                Document.Class = "";
+            }
+
+            do
+            {
+                index = ParseFieldName(index, null);
+            }
+            while (index < RawDocument.Length);
+
+            return Document;
+        }
+
+        private int ParseFieldName(int i, Dictionary<string, object> fields)
+        {
+            int startIndex = i;
+
+            while (RawDocument[i] != ':')
+            {
+                i++;
+            }
+
+            Dictionary<string, object> currentDocument;
+            string fieldName = RawDocument.Substring(startIndex, i - startIndex);
+
+            if (fields == null)
+            {
+                currentDocument = Document.Fields;
+            }
+            else
+            {
+                currentDocument = fields;
+            }
+
+            currentDocument.Add(fieldName, null);
+
+            // move to position after colon (:)
+            i++;
+
+            // check if it's not the end of document which means that current field has null value
+            if (i == RawDocument.Length)
+            {
+                return i;
+            }
+
+            switch (RawDocument[i])
+            {
+                case '"':
+                    i = ParseString(i, currentDocument, fieldName);
+                    break;
+                case '#':
+                    i = ParseRecordID(i++, currentDocument, fieldName);
+                    break;
+                case '(':
+                    i = ParseEmbeddedDocument(i, currentDocument, fieldName);
+                    break;
+                /*case '[':
+                    i = ParseCollection(i);
+                    break;*/
+                default:
+                    break;
+            }
+
+            // single string value was parsed and we need to push the index if next character is comma
+            if (RawDocument[i] == ',')
+            {
+                i++;
+            }
+
+            return i;
+        }
+
+        private int ParseString(int i, Dictionary<string, object> fields, string fieldName)
+        {
+            // move to the inside of string
+            i++;
+
+            int startIndex = i;
+
+            while (RawDocument[i] != '"')
+            {
+                i++;
+            }
+
+            fields[fieldName] = RawDocument.Substring(startIndex, i - startIndex);
+            i++;
+
+            return i;
+        }
+
+        private int ParseRecordID(int i, Dictionary<string, object> fields, string fieldName)
+        {
+            int startIndex = i;
+
+            while ((RawDocument[i] != ',') && (RawDocument[i] != ')') && (i < RawDocument.Length))
+            {
+                i++;
+            }
+
+            fields[fieldName] = RawDocument.Substring(startIndex, i - startIndex);
+
+            return i;
+        }
+
+        private int ParseEmbeddedDocument(int i, Dictionary<string, object> fields, string fieldName)
+        {
+            // move to the inside of embedded document
+            i++;
+
+            int startIndex = i;
+
+            Dictionary<string, object> embeddedDocument = new Dictionary<string, object>();
+            fields[fieldName] = embeddedDocument;
+
+            while (RawDocument[i] != ')')
+            {
+                i = ParseFieldName(i, embeddedDocument);
+            }
+
+            // move past close bracket of embedded document
+            i++;
+
+            return i;
         }
     }
 }
