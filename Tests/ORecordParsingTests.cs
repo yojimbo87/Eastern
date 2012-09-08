f133501b4cfbe845ce7bf000bc8a3dfc66048b52
Tomas Bosak <bosak.tomas@gmail.com>
2012-09-08 17:37:51 +0200

    Tests for boolean parsing.

--- Tests/ORecordParsingTests.cs
+++ Tests/ORecordParsingTests.cs
@@ -57,5 +57,36 @@ namespace Tests
             Assert.IsTrue(arrayDates.Last().GetType() == typeof(DateTime));
             Assert.IsTrue((DateTime)arrayDates.Last() == new DateTime(2011, 5, 25, 0, 0, 0));
         }
+
+        [TestMethod]
+        public void TestBoolean()
+        {
+            string raw = "singleT:true,singleF:false,embedded:(singleT:true,singleF:false),array:[true,false]";
+
+            ORecord record = new ORecord(ORecordType.Document, 0, UTF8Encoding.UTF8.GetBytes(raw));
+            ODocument document = record.ToDocument();
+
+            Assert.IsTrue(document.Fields["singleT"].GetType() == typeof(bool));
+            Assert.IsTrue((bool)document.Fields["singleT"] == true);
+
+            Assert.IsTrue(document.Fields["singleF"].GetType() == typeof(bool));
+            Assert.IsTrue((bool)document.Fields["singleF"] == false);
+
+            Dictionary<string, object> embedded = (Dictionary<string, object>)document.Fields["embedded"];
+
+            Assert.IsTrue(embedded["singleT"].GetType() == typeof(bool));
+            Assert.IsTrue((bool)embedded["singleT"] == true);
+
+            Assert.IsTrue(embedded["singleF"].GetType() == typeof(bool));
+            Assert.IsTrue((bool)embedded["singleF"] == false);
+
+            List<object> array = (List<object>)document.Fields["array"];
+
+            Assert.IsTrue(array.First().GetType() == typeof(bool));
+            Assert.IsTrue((bool)array.First() == true);
+
+            Assert.IsTrue(array.Last().GetType() == typeof(bool));
+            Assert.IsTrue((bool)array.Last() == false);
+        }
     }
 }
