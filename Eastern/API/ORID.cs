d16a446c9c92e033b3388630e1eefb85641d50c2
Tomas Bosak <bosak.tomas@gmail.com>
2012-09-25 18:57:21 +0200

    Implement ORID class.

---
+++ Eastern/API/ORID.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Eastern
+{
+    public class ORID
+    {
+        public short ClusterID { get; set; }
+        public long ClusterPosition { get; set; }
+        public string RID 
+        {
+            get
+            {
+                return ClusterID + ":" + ClusterPosition;
+            }
+
+            set
+            {
+                string[] split = value.Split(':');
+
+                ClusterID = short.Parse(split[0]);
+                ClusterPosition = long.Parse(split[1]);
+            } 
+        }
+    }
+}
