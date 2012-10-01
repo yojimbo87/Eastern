901a25dfb74abd2ce84fd76746cbf84bb3419c52
Tomas Bosak <bosak.tomas@gmail.com>
2012-10-01 22:27:04 +0200

    Create static RecordParser class which will be responsible for marshalling and unmarshalling.

---
+++ Eastern/Protocol/RecordParser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Reflection;
+
+namespace Eastern.Protocol
+{
+    internal static class RecordParser
+    {
+        internal static string MarshalObject<T>(T objectToMarshal) 
+        {
+            string marshaledObjectString = "";
+
+            Type type = objectToMarshal.GetType();
+
+            foreach (PropertyInfo property in type.GetProperties())
+            {
+                
+            }
+
+            return marshaledObjectString;
+        }
+
+        //private static string MarshalEmbeddedObject
+    }
+}
