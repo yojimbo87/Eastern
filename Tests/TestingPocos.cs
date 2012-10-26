5febc3cdf7288c581131bf6cc8a98ae264d097b4
Tomas Bosak <bosak.tomas@gmail.com>
2012-10-26 14:54:58 +0200

    Add testing POCOs to separate file since they are used across the tests.

---
+++ Tests/TestingPocos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tests
+{
+    class TestClass
+    {
+        public string Null { get; set; }
+        public bool IsBool { get; set; }
+        public byte ByteNumber { get; set; }
+        public short ShortNumber { get; set; }
+        public int IntNumber { get; set; }
+        public long LongNumber { get; set; }
+        public float FloatNumber { get; set; }
+        public double DoubleNumber { get; set; }
+        public decimal DecimalNumber { get; set; }
+        public DateTime DateTime { get; set; }
+        public string String { get; set; }
+        public string[] StringArray { get; set; }
+        public List<string> StringList { get; set; }
+        public TestNestedClass NestedClass { get; set; }
+        public List<TestNestedClass> ObjectList { get; set; }
+    }
+
+    class TestNestedClass
+    {
+        public string NestedString { get; set; }
+        public string[] StringArray { get; set; }
+        public List<string> StringList { get; set; }
+    }
+}
